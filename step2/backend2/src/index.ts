b4987632792fa4426522cb2a33a59471d1c3e9f8
shieldx <anhnv.24810310060@epu.edu.vn>
2025-12-15 14:24:07 +0700

    fix: update Jenkinsfile to enhance deployment process and streamline image tagging

--- step2/backend2/src/index.ts
+++ step2/backend2/src/index.ts
@@ -1,13 +1,13 @@
-    // src/index.ts
-    import express, { Request, Response } from 'express';
+// src/index.ts
+import express, { Request, Response } from 'express';
 
-    const app = express();
-    const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-    app.get('/', (req: Request, res: Response) => {
-      res.send('Hello from Express with TypeScript!');
-    });
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from Express with TypeScript!');
+});
 
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}, Hello  World Typescript! GitOps V1!`);
+});
